af0aa62d3a1dc8a57d3bb8c558d01eb29556b91e
Kornel Żaba <kornelzaba@gmail.com>
2018-09-04 20:14:53 +0200

    Added echo effect

--- effect.py
+++ effect.py
@@ -23,5 +23,42 @@ class Noise(Effect):
             signal = self.effect.apply(signal)
 
         noise = np.random.normal(self.mu, self.sigma, signal.shape)
-        return signal + noise
+        return signal + noise.astype(np.int16)
 
+
+class Echo(Effect):
+
+    MIN_VOLUME = 0.1
+
+    def __init__(self, sample_rate, delay, decay, repeats, prolong, effect=None):
+        super(Echo, self).__init__(effect)
+        self.sample_rate = sample_rate
+        self.delay = delay
+        self.decay = decay
+        self.repeats = repeats
+        self.prolong = prolong
+
+    def apply(self, signal):
+        if self.effect is not None:
+            signal = self.effect.apply(signal)
+
+        samples_to_delay = int(self.delay * self.sample_rate)
+        new_signal = copy_buffer(signal, samples_to_delay * self.repeats, self.prolong)
+        decays = np.linspace(self.decay, Echo.MIN_VOLUME, self.repeats)
+        for i in range(signal.shape[0]):
+            for j in range(self.repeats):
+                if i + (samples_to_delay * j) > new_signal.shape[0] and not self.prolong:
+                    break
+                new_signal[i + (samples_to_delay * j)] += int(signal[i] * decays[j])
+        return new_signal
+
+
+def copy_buffer(original, added_length, prolong):
+    if prolong:
+        length = list(original.shape)
+        length[0] += added_length
+        new_signal = np.zeros(length, dtype=np.int16)
+    else:
+        new_signal = np.zeros(original.shape, dtype=np.int16)
+    new_signal[:original.shape[0]] += original
+    return new_signal
--- wav_io.py
+++ wav_io.py
@@ -1,5 +1,6 @@
 import scipy.io.wavfile
 import numpy as np
+import effect
 
 
 def read_wav(path):
@@ -10,3 +11,17 @@ def write_wav(path, data, sample_rate):
     data = data.astype(np.int16) #type int16 signed wav file
     scipy.io.wavfile.write(path, sample_rate, data)
 
+
+rate, wav = read_wav("KZ2.wav")
+echo = effect.Echo(rate, 0.99, 0.40, 5, True)
+write_wav("echoed.wav", echo.apply(wav), rate)
+
+rate, wav = read_wav("KZ2.wav")
+echo = effect.Echo(rate, delay=0.80, decay=0.40, repeats=2, prolong=True)
+noise = effect.Noise(mu=0, sigma=25, effect=echo)
+write_wav("echo_then_noise.wav", noise.apply(wav), rate)
+
+rate, wav = read_wav("KZ2.wav")
+noise = effect.Noise(mu=0, sigma=25)
+echo = effect.Echo(rate, delay=0.80, decay=0.40, repeats=2, prolong=True, effect=noise)
+write_wav("noise_then_echo.wav", echo.apply(wav), rate)
