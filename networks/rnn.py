a70ca271e9efb595c2252f5a973621cd1bede9e7
Kornel Żaba <kornelzaba@gmail.com>
2018-11-15 21:59:50 +0100

    Finished rnn

--- networks/rnn.py
+++ networks/rnn.py
@@ -5,78 +5,111 @@ from networks import network_util
 class Rnn(object):
 
     def __init__(self, num_inputs, num_classes, layers, dropout, epochs,
-                 learning_rate, save_model, save_path):
+                 learning_rate, batch_size, save_model, save_path, max_seq_len):
         self.num_inputs = num_inputs
         self.num_classes = num_classes
         self.layers = layers
         self.dropout = dropout
         self.epochs = epochs
         self.learning_rate = learning_rate
-        #self.batch_size = batch_size
+        self.batch_size = batch_size
         self.save_model = save_model
         self.save_path = save_path
+        self.max_seq_len = max_seq_len
 
-    def save(self):
-        pass
+        self.x = None
+        self.y = None
+        self.sequence_length = None
+        self.weight = None
+        self.bias = None
+        self.net = None
+        self.predictions = None
+        self.loss = None
+        self.accuracy = None
+        self.global_step = None
+        self.optimizer = None
+        self.session = None
+        self.saver = None
 
-    def test(self):
-        pass
+    def _save(self, session):
+        if self.save_model:
+            self.saver.save(session, self.save_path, self.epochs)
+
+    def _load(self, session, model_path):
+        if self.save_path is not None:
+            self.saver = tf.train.import_meta_graph(model_path)
+            self.saver.restore(session, tf.train.latest_checkpoint("tmp/")) #pass config path?
 
     def train(self, train_data, train_labels):
+        for epoch in range(self.epochs):
+            feed_dict = {
+                self.x: train_data,
+                self.y: train_labels
+            }
+            _, train_step, train_predictions, train_loss, train_accuracy = self.session.run(
+                [self.optimizer, self.global_step, self.predictions, self.loss, self.accuracy], feed_dict
+            )
+            print("Step: {}, loss {}, acc {}".format(train_step, train_loss, train_accuracy))
+
+    def test(self, test_data, test_labels, model_path):
+        with tf.Session() as session:
+            self._load(session, model_path)
+            feed_dict = {
+                self.x: test_data,
+                self.y: test_labels
+            }
+            _, result, test_accuracy = self.session.run(
+                [self.net, self.predictions, self.accuracy], feed_dict
+            )
+            print("Test accuracy:", test_accuracy)
+            print("Result\n {} {}".format(_, result))
+        return result, test_accuracy
+
+    def build(self):
         graph = tf.Graph()
         with graph.as_default():
-            max_len = network_util.get_max_sequence_length(train_data)
-
-            x = tf.placeholder(tf.float32,
-                shape=[None, max_len, self.num_inputs],
+            self.x = tf.placeholder(tf.float32,
+                shape=[None, self.max_seq_len, self.num_inputs],
             )
-            sequence_len = network_util.get_true_sequence_length(x, axis=1)
-            y = tf.placeholder(tf.float32,
+            self.sequence_length = network_util.get_true_sequence_length(self.x, axis=1)
+            self.y = tf.placeholder(tf.float32,
                 shape=[None, self.num_classes],
             )
-            #dropout_keep_probability = tf.placeholder(tf.float32)
 
-            weight = {
+            self.weight = {
                 "out": tf.Variable(tf.truncated_normal(
-                    [self.layers[0], self.num_classes]
+                    [self.layers[-1], self.num_classes]
                 ))
             }
-            bias = {
+            self.bias = {
                 "out": tf.Variable(tf.zeros([self.num_classes]))
             }
+
             def rnn(x, w, b, seq_len):
-                rnn_cell = tf.contrib.rnn.BasicRNNCell(self.layers[0])
-                out, _ = tf.nn.dynamic_rnn(rnn_cell, x,
-                                           sequence_length=sequence_len,
+                rnn_cells = [tf.contrib.rnn.BasicRNNCell(layer) for layer in self.layers]
+                rnn_cells = tf.contrib.rnn.MultiRNNCell(rnn_cells)
+                out, _ = tf.nn.dynamic_rnn(rnn_cells, x,
+                                           sequence_length=seq_len,
                                            dtype=tf.float32)
                 out = network_util.transpose_to_last_values(out, seq_len)
                 return tf.nn.softmax(tf.matmul(out, w["out"]) + b["out"])
 
-            logits = rnn(x, weight, bias, sequence_len)
-            pred = tf.argmax(logits, axis=1)
-            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
-                logits=logits, labels=y
+            self.net = rnn(self.x, self.weight, self.bias, self.sequence_length)
+            self.predictions = tf.argmax(self.net, axis=1)
+            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
+                logits=self.net, labels=self.y
             ))
-            accuracy = tf.reduce_mean(
-                tf.cast(tf.equal(pred, tf.argmax(y, axis=1)), dtype=tf.float32)
+            self.accuracy = tf.reduce_mean(
+                tf.cast(tf.equal(self.predictions, tf.argmax(self.y, axis=1)), dtype=tf.float32)
             )
-            step = tf.Variable(0, trainable=False)
-            optimizer = tf.train.AdamOptimizer(self.learning_rate)\
-                .minimize(loss, global_step=step)
-
-            session = tf.Session()
-            with session.as_default():
-                session.run(tf.global_variables_initializer())
-
-                for epoch in range(self.epochs):
-                    feed_dict = {
-                        x: train_data,
-                        y: train_labels
-                    }
-                    _, c_step, c_loss, c_accuracy = session.run(
-                        [optimizer, step, loss, accuracy], feed_dict
-                    )
-                    print("Epoch {}: step {}, loss {}, acc {}".format(epoch, c_step, c_loss, c_accuracy))
+            self.global_step = tf.Variable(0, trainable=False)
+            self.optimizer = tf.train.AdamOptimizer(self.learning_rate)\
+                .minimize(self.loss, global_step=self.global_step)
+            self.saver = tf.train.Saver()
+            self.session = tf.Session()
+            with self.session.as_default():
+                self.session.run(tf.global_variables_initializer())
+                self._save(self.session)
 
 
 
--- tests/network_test.py
+++ tests/network_test.py
@@ -5,12 +5,14 @@ def rnn_test():
     net = rnn.Rnn(
         num_inputs=2,
         num_classes=3,
-        layers=[10],
+        layers=[20, 10],
         dropout=None,
-        epochs=20,
-        learning_rate=0.01,
-        save_model=False,
-        save_path=None
+        epochs=100,
+        learning_rate=0.001,
+        batch_size=4,
+        save_model=True,
+        save_path="tmp/model.tf",
+        max_seq_len=4
     )
     input = np.array([
         [[1, 2], [1, 2], [1, 4], [1, 5]],
@@ -20,7 +22,12 @@ def rnn_test():
     ], dtype=np.float32)
     labels = network_util.one_hot_encode(np.array([0, 1, 0, 2]),
                                          num_classes=3)
-
+    net.build()
     net.train(input, labels)
+    test_input = np.array([[[-5, 0], [-6, 0], [-5, 0], [0, 0]]],
+                          dtype=np.float32)
+    labels = network_util.one_hot_encode(np.array([2]), num_classes=3)
+    net.test(test_input, labels, "tmp/model.tf-100.meta")
+
 
 rnn_test()
